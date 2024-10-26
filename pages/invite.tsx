60575d81bc15100f0fd4b0d85ae37f736558bd5e
Fabian Weber <frznhope@gmail.com>
2024-10-26 16:34:45 +0400

    some more ui

--- pages/invite.tsx
+++ pages/invite.tsx
@@ -1,13 +1,38 @@
 import type { NextPage } from "next";
 import { Button } from "@mui/material";
-import FrameComponent3 from "./frame-component3";
-import styles from "./index.module.css";
+import FrameComponent3 from "../components/frame-component3";
+import styles from "./invite.module.css";
+import { useChain } from "@cosmos-kit/react";
+import { WalletStatus } from "cosmos-kit";
+import { affiliate } from "@/utils/cosmwasm";
 
 export type InvitedType = {
   className?: string;
 };
 
 const Invited: NextPage<InvitedType> = ({ className = "" }) => {
+  const {
+    status,
+    connect,
+    address,
+    getSigningCosmWasmClient
+  } = useChain("osmosis");
+
+  const handleClick = async () => {
+    if (status === WalletStatus.Disconnected) {
+      connect();
+      return;
+    }
+    if (status === WalletStatus.Connected) {
+      // send message to affiliate
+      const signingClient = await getSigningCosmWasmClient();
+      const refferer = window.location.search.substring(1).split("&").find((param) => param.startsWith("code"))?.split("=")[1];
+      // @ts-ignore
+      await affiliate(signingClient, address, refferer);
+      return
+    }
+  }
+
   return (
     <div className={[styles.invited, className].join(" ")}>
       <div className={styles.topSpacer} />
@@ -42,8 +67,9 @@ const Invited: NextPage<InvitedType> = ({ className = "" }) => {
               width: 193,
               height: 54,
             }}
+            onClick={handleClick}
           >
-            Connect Wallet
+            {status === WalletStatus.Disconnected ? "Connect Wallet" : status === WalletStatus.Connected ? "Join Symbiosis" : "Loading..."}
           </Button>
           <div className={styles.andStartEarning}>And start earning</div>
         </div>
